809257325be010c72b3bba18d63516751b9b2390
noalerch <noa.lerch@omegapoint.se>
2024-10-13 16:51:24 +0200

    add layout

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
-
 <h1>Noa Lerch</h1>
 <h2>Swedish computer programmer and all around fun guy</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- app/src/routes/about/+page.svelte
+++ app/src/routes/about/+page.svelte
@@ -1,8 +1,3 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
-
 <p>
     My name is Noa Lerch and I am a software developer from Sweden.
 </p>
