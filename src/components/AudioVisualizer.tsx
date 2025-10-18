e250601f964d08ed2b99cc8bf407ac4dd4f7a2cd
Noa Lerch <noazlerch@gmail.com>
2025-10-18 09:49:05 +0200

    Add vector logic

--- src/components/AudioVisualizer.tsx
+++ src/components/AudioVisualizer.tsx
@@ -3,12 +3,6 @@ export default function AudioVisualizer() {
   if (typeof window === "undefined") return;
 
   import("p5").then(({ default: p5 }) => {
-    interface Circle {
-        x: number;
-        y: number;
-        size: number;
-        alpha: number;
-      }
 
     let circles: Circle[] = [];
 
@@ -21,12 +15,22 @@ export default function AudioVisualizer() {
       p.draw = () => {
         p.background(0, 20);
         p.fill(150);
-        circles.push({x: p.mouseX, y: p.mouseY, size: 20, alpha: 100});
+        const mousePosition = new Vector(p.mouseX, p.mouseY)
+
+        circles.push(new Circle(
+          mousePosition, new Vector(), new Vector(), 20, 150
+        ))
+
+        const accFactor = 0.4
 
         // update and draw circles
         for (let i = circles.length - 1; i >= 0; i--) {
           const c = circles[i];
           c.alpha -= 1; 
+
+
+          const directionToMouse: Vector = Vector.subtract(mousePosition, c.position)
+          const acceleration: Vector = Vector.scale(accFactor, directionToMouse)
 
           p.noStroke();
           p.fill(255, 150, 50, c.alpha);
@@ -41,7 +45,7 @@ export default function AudioVisualizer() {
       };
     };
 
-    const moveLeft = (x: Number) => {
+    const moveLeft = (x: number) => {
         return x - 8
     }
 
@@ -49,4 +53,44 @@ export default function AudioVisualizer() {
 
 
   });
+}
+
+class Circle {
+  constructor(
+    public position: Vector,
+    public velocity: Vector,
+    public acceleration: Vector,
+    public size: number,
+    public alpha: number
+  ) {}
+}
+
+class Vector {
+  x: number
+  y: number
+
+  constructor(x: number = 0, y: number = 0) {
+    this.x = x
+    this.y = y
+  }
+
+  static normalize(u: Vector) {
+    const length: number = Math.hypot(u.x, u.y)
+    if (length > 0) {
+        return Vector.scale(1 / length, u)
+    }
+    return new Vector()
+  }
+
+  static add(u: Vector, v: Vector) {
+    return new Vector(u.x + v.x, u.y + v.y)
+  }
+
+  static subtract(u: Vector, v: Vector) {
+    return new Vector(u.x - v.x, u.y - v.y)
+  }
+
+  static scale(s: number, v: Vector) {
+    return new Vector(s * v.x, s * v.y)
+  }
 }
