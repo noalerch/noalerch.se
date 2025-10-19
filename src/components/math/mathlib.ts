faad547da7791b1644b9d1798e7be49d746a7cb2
Noa Lerch <noazlerch@gmail.com>
2025-10-19 16:14:17 +0200

    Revert "wip"
    This reverts commit 87ced94ab4a81c337763358d4d359cb60ec3a167.

--- src/components/math/mathlib.ts
+++
@@ -1,6 +0,0 @@
-
-export type Real2D = (x: number, y: number) => number
-
-export class Multivar2D {
-    constructor(public name: string, public f: Real2D, public df_dx: Real2D, public df_dy: Real2D) {}
-}
